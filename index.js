4ae99680c0aabdcdf3061cd906b1c22c33905487
Vinícius Oliveira Gonçalves <viniciusoligon@outlook.com>
2020-10-22 16:10:05 -0300

    igualando ao código da aula

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
 
 const port = 3000;
- 
-//precisamos avisar o Express para utilizar o body-parser
+
+// Precisamos avisar o Express para utilizar o body-parser
+// Assim, ele saberá como transformar as informações no BODY da requisição
+//      em informação útil para a programação
 
 app.use(bodyParser.json());
+
+/*
+-> Create, Read (All/Single), Update & Delete
+-> Criar, Ler (Tudo ou Individual), Atualizar e Remover
+*/
+
+/*
+URL -> http://localhost:3000
+Endpoint ou Rota -> [GET] /mensagem
+Endpoint ou Rota -> [POST] /mensagem
+Endpoint: [GET] /mensagem
+Descrição: Ler todas as mensagens
+Endpoint: [POST] /mensagem
+Descrição: Criar uma mensagem
+Endpoint: [GET] /mensagem/{id}
+Descrição: Ler mensagem específica pelo ID
+Exemplo: [GET] /mensagem/1
+Endpoint: [PUT] /mensagem/{id}
+Descrição: Edita mensagem específica pelo ID
+Endpoint: [DELETE] /mensagem/{id}
+Descrição: Remove mensagem específica pelo ID
+*/
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-const mensagens = ['primeira mensagem', 'segunda mensagem'];
+const mensagens = ['Essa é uma mensagem', 'Essa é outra mensagem'];
 
-//ler todas as msgs
-
-app.get('/mensagens', function(req, res){
+// Read All
+app.get('/mensagem', function (req, res) {
     res.send(mensagens);
 });
 
-//Criar
-app.post('/mensagens', function(req,res){
-    res.send('Criar mensagem');
+// Create
+app.post('/mensagem', function (req, res) {
+    // Paramos aqui:
+    // res.send(req.body);
+
+    // Obtenho o texto a partir do body da requisição
+    const texto = req.body.texto;
+
+    // Adiciono o texto recebido na lista de mensagens.
+    mensagens.push(texto);
+
+    res.send('Mensagem adicionada com sucesso.');
 });
- 
-app.listen(port, function(){
-console.log('App rodando em http://localhost:' + port);
+
+app.listen(port, function () {
+    console.log('App rodando em http://localhost:' + port);
 });
-
-
-
